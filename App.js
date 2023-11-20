eb7c9a35d57ae13959bc265fe8b6747941b68467
mihailden <midenisenko@gmail.com>
2023-11-20 23:30:34 +0200

    add getResource logic at component did mount

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+
+import Loading from './src/components/Loading';
+import Weather from './src/components/Weather';
+import { getLocationResource, getWeatherResource, setAlert } from './src/utils/api';
 
 export default function App() {
+    const [loading, setLoading] = useState(true);
+    const [temperature, setTemperature] = useState(null);
+    
+    useEffect(() => {
+        (
+            async () => {
+                const userLocation = await getLocationResource();
+                !userLocation && setAlert('Couldn\'t get a location', 'very sorry');
+                
+                const res = await getWeatherResource(userLocation.coords);
+                !res && setAlert('Couldn\'t load weather', 'very sorry');
+                
+                const {temp, temp_max, temp_min} = res.data.main;
+                
+                setTemperature({
+                    temp: Math.round(temp),
+                    temp_max: Math.round(temp_max),
+                    temp_min: Math.round(temp_min),
+                });
+                setLoading(!res || !userLocation);
+            }
+        )();
+    }, []);
+    
     return (
-        <View style={ styles.container }>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto"/>
-        </View>
+        loading ? <Loading/> : <Weather temperature={temperature}/>
     );
 }
 
